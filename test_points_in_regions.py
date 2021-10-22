5b7a63a10a3e6d15cf869fa2ba37ee2941e4e2bb
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2021-10-22 17:50:17 +0100

    Add region-in-region testing

--- test_points_in_regions.py
+++ test_points_in_regions.py
@@ -14,7 +14,12 @@ class Range:
     start: float
     end: float
 
-    def __contains__(self, value: float) -> bool:
+    def __contains__(self, value: Union[float, Range, Stops]) -> bool:
+        if isinstance(value, Range):
+            start, end = sorted((value.start, value.end))
+            return self.start <= start <= self.end and self.start <= end <= self.end
+        if isinstance(value, Stops):
+            return all(self.start <= stop <= self.end for stop in value.stops)
         return self.start <= value <= self.end
 
 
@@ -24,7 +29,11 @@ class Stops:
 
     stops: set[float]
 
-    def __contains__(self, value: float) -> bool:
+    def __contains__(self, value: Union[float, Range, Stops]) -> bool:
+        if isinstance(value, Range):
+            return value.start == value.end and value.start in self.stops
+        if isinstance(value, Stops):
+            return all(stop in self.stops for stop in value.stops)
         return value in self.stops
 
 
@@ -33,8 +42,8 @@ Region = Mapping[str, Union[Range, Stops]]
 
 
 def location_in_region(location: Location, region: Region) -> bool:
-    return all(
-        name in region and value in region[name] for name, value in location.items()
+    return location.keys() == region.keys() and all(
+        value in region[name] for name, value in location.items()
     )
 
 
@@ -56,3 +65,52 @@ def test_location_outside_region(a: float, b: float, c: float) -> None:
     loc: Location = {"a": a, "b": b, "c": c}
     region: Region = {"a": Range(100, 900), "b": Range(75, 125), "c": Stops({0, 1})}
     assert not location_in_region(loc, region)
+
+
+def region_in_region(region1: Region, region2: Region) -> bool:
+    return region1.keys() == region2.keys() and all(
+        value in region2[name] for name, value in region1.items()
+    )
+
+
+@given(
+    a1=st.floats(min_value=100, max_value=900),
+    a2=st.floats(min_value=100, max_value=900),
+    b1=st.floats(min_value=75, max_value=125),
+    b2=st.floats(min_value=75, max_value=125),
+    c1=st.sampled_from([0, 1]),
+    c2=st.sampled_from([0, 1]),
+)
+def test_region_in_region(
+    a1: float, a2: float, b1: float, b2: float, c1: float, c2: float
+) -> None:
+    region1: Region = {"a": Range(a1, a2), "b": Range(b1, b2), "c": Stops({c1, c2})}
+    region2: Region = {"a": Range(100, 900), "b": Range(75, 125), "c": Stops({0, 1})}
+    assert region_in_region(region1, region2)
+
+
+@given(
+    a1=st.floats(),
+    a2=st.floats(),
+    b1=st.floats(),
+    b2=st.floats(),
+    c1=st.floats(),
+    c2=st.floats(),
+)
+def test_region_outside_region(
+    a1: float, a2: float, b1: float, b2: float, c1: float, c2: float
+) -> None:
+    assume(
+        not (
+            100 <= a1 <= 900
+            and 100 <= a2 <= 900
+            and 75 <= b1 <= 125
+            and 75 <= b2 <= 125
+            and c1 in {0, 1}
+            and c2 in {0, 1}
+        )
+    )
+
+    region1: Region = {"a": Range(a1, a2), "b": Range(b1, b2), "c": Stops({c1, c2})}
+    region2: Region = {"a": Range(100, 900), "b": Range(75, 125), "c": Stops({0, 1})}
+    assert not region_in_region(region1, region2)
